c69a0c94f59a55d69e4608ae15df3782c020d5c8
Amine Kadoum <ma_kadoum@esi.dz>
2024-12-28 11:41:47 +0100

    liste consultations

--- src/app/components/consultations/consultations.component.ts
+++ src/app/components/consultations/consultations.component.ts
@@ -1,7 +1,18 @@
 import { RouterLink , RouterModule, RouterOutlet } from '@angular/router';
-import { CommonModule  } from '@angular/common';
+import { CommonModule, Time  } from '@angular/common';
 import { Component } from '@angular/core';
+interface DataRow {
+  date: Date;
+  heure: String;
+  ordonnance: string;
+  diagnostic: string;
+  resume: string;
+}
 
+interface Column {
+  key: keyof DataRow;
+  label: string;
+}
 @Component({
   selector: 'app-consultations',
   standalone: true,
@@ -10,5 +21,24 @@ import { Component } from '@angular/core';
   styleUrl: './consultations.component.scss'
 })
 export class ConsultationsComponent {
+
+  columns: Column[] = [
+    { key: 'date', label: 'Date' },
+    { key: 'heure', label: 'Heure' },
+    { key: 'ordonnance', label: 'Ordonnance' },
+    { key: 'diagnostic', label: 'Diagnostic' },
+    { key: 'resume', label: 'Resume' },
+  ];
+
+  rows: DataRow[] = [
+    { date: new Date(), heure: '10:00', ordonnance: 'ordonnance', diagnostic: 'diagnostic', resume: 'resume' },
+    { date: new Date(), heure: '11:00', ordonnance: 'ordonnance', diagnostic: 'diagnostic', resume: 'resume' },
+    { date: new Date(), heure: '12:00', ordonnance: 'ordonnance', diagnostic: 'diagnostic', resume: 'resume' },
+    { date: new Date(), heure: '13:00', ordonnance: 'ordonnance', diagnostic: 'diagnostic', resume: 'resume' },
+    { date: new Date(), heure: '14:00', ordonnance: 'ordonnance', diagnostic: 'diagnostic', resume: 'resume' },
+  ];
+
+  
+  
 
 }
